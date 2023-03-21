a69cd3aaca8cb04b459db16febc9f8806effe4ad
Mehdi van Loon <mehdivanloon123@gmail.com>
2023-03-21 11:57:20 +0100

    CORS handling

--- src/index.php
+++ src/index.php
@@ -6,6 +6,8 @@ require "includes/Todos.class.php";
 // Set the HTTP response headers
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS");
 
 
 
